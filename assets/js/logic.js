02e2cbf5f83fc9a3287d91bb5aa681c7a6a0004d
Ginanjar Pamungkas <ginanjar69919082@gmail.com>
2020-06-05 10:39:44 +0700

    portofolio upload

---
+++ assets/js/logic.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#nama').addClass('bounceIn').removeClass('hide')
+    setTimeout(function(){$('#webDev').addClass('pulse').removeClass('hide')}, 550);
+    setTimeout(function(){$('#education').addClass('fadeInLeft').removeClass('hide');$('#image').addClass('fadeInRight').removeClass('hide')}, 750);
+    setTimeout(function(){$('#eduText').addClass('fadeInLeft').removeClass('hide')}, 950);
+    setTimeout(function(){$('#workExp').addClass('fadeInLeft').removeClass('hide');$('#footer').addClass('fadeInRight').removeClass('hide')}, 1150);
+    setTimeout(function(){$('#workExpText').addClass('fadeInLeft').removeClass('hide');$('#imagePict').addClass('rotateIn').removeClass('hide')}, 1350);
+    setTimeout(function(){$('#skill').addClass('fadeInLeft').removeClass('hide');$('#birthDate').addClass('jackInTheBox').removeClass('hide')}, 1550);
+    setTimeout(function(){$('#skillText').addClass('fadeInLeft').removeClass('hide');$('#summaryText').addClass('bounceIn').removeClass('hide');$('#onlinePort').addClass('bounceIn').removeClass('hide')}, 1750);
+    setTimeout(function(){$('.link').addClass('pulse').removeClass('hide');$('#contact').addClass('bounceIn').removeClass('hide')}, 1950);
+    setTimeout(function(){$('.title').addClass('pulse').removeClass('hide');$('.line').addClass('zoomIn').removeClass('hide')}, 2150);
+})
